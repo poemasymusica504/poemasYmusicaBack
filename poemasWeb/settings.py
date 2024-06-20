1fde3c2ef2d687cccd07ec1fa9aa8b7c13826a59
David Agudelo <davidagudelo@tecmovin.com>
2024-06-20 06:18:10 -0500

    cambios en allowed host

--- poemasWeb/settings.py
+++ poemasWeb/settings.py
@@ -24,6 +24,10 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['http://localhost:5173',])
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
